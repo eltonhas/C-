c3a24a9295a82e29d089b3178da064e9f07af032
eltonhas <71413887+eltonhas@users.noreply.github.com>
2020-09-16 20:38:30 -0300

    Add files via upload

---
+++ Empregados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ExercicioEmpregados {
+    class Empregados {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Empregados (int id, string nome, double salario) {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void aumentarSalario (double porc) {
+            double incremento = this.Salario * (porc / 100);
+            this.Salario += incremento;
+        }
+
+        public override string ToString() {
+            return $"{Id}, {Nome}, {Salario.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ExercicioEmpregados {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Quantos empregados deseja cadastrar? ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            List<object> listaEmpregados = new List<object>();
+
+            for (int i = 1; i <= qtd; i++) {
+                Console.WriteLine($"Empregado #{i}");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                listaEmpregados.Add(new Empregados(id, nome, salario));
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("------------------------"); 
+            Console.WriteLine();
+
+            Console.Write("Digite o ID do funcionário para atualizar o salário: ");
+            int idEmp = int.Parse(Console.ReadLine());
+
+            Empregados emp = listaEmpregados.Find(x => x.Id == idEmp);
+
+            if (emp != null) {
+                Console.Write("Digite a porcentagem do aumento: ");
+                double porc = double.Parse(Console.ReadLine());
+
+                emp.aumentarSalario(porc);
+            } else {
+                Console.WriteLine("Esse Id não existe!");
+            }
+
+
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Lista de empregados: ");
+            foreach( object obj in listaEmpregados) {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
